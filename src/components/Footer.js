7de63712dfba72a0150361c5a39425e5e0089a2f
genzaraky <amadou140589@gmail.com>
2020-11-30 15:46:59 +0100

    footerbar added.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,52 @@
 import React from 'react';
-
+import Logo from '../logo.png';
 export default function Footer() {
 
     return (
-        <footer className="footer">
-            <div className="container">
+        <React.Fragment>
 
-                <nav className="level">
-                    <div className="level-left">
-                        <div className="level-item">
-                            <a  className="has-text-black">Facebook</a>
-                          
+            <footer className="footer">
+                <div className="container">
+
+                    <nav className="level">
+                        <div className="level-left">
+                            <div className="level-item">
+                                <a  className="has-text-black">Facebook</a>
+                            
+                            </div>
+
+                            <div className="level-item">
+                            
+                                <a  className="has-text-black">Instagram</a>
+                            </div>
+                            <div className="level-item">
+                            
+                                <a  className="has-text-black">Youtube</a>
+                            </div>
                         </div>
 
-                        <div className="level-item">
-                           
-                            <a  className="has-text-black">Instagram</a>
+                        <div className="level-right">
+                            <p className="level-item "><a className="has-text-black"> Qui sommes-nous?</a></p>
+                            <p className="level-item"><a className="has-text-black">Devenir Membre</a></p>
+                            <p className="level-item"><a className="has-text-black">Calendrier des activités</a></p>
+                            <p className="level-item"><a className="has-text-black">Contacts</a></p>
                         </div>
-                        <div className="level-item">
-                           
-                            <a  className="has-text-black">Youtube</a>
-                        </div>
+                    </nav>
+                </div>
+            </footer>
+            <div className="footer_bar">
+                    <div className="container has-text-centered">
+                        <p>
+
+                        <img src={Logo} alt="" width="200"/>
+                        </p>
+                        <p>
+                        Copyright © 2020 - Champion Chretien
+
+                        </p>
                     </div>
 
-                    <div className="level-right">
-                        <p className="level-item "><a className="has-text-black"> Qui sommes-nous?</a></p>
-                        <p className="level-item"><a className="has-text-black">Devenir Membre</a></p>
-                        <p className="level-item"><a className="has-text-black">Calendrier des activités</a></p>
-                        <p className="level-item"><a className="has-text-black">Contacts</a></p>
-                    </div>
-                </nav>
             </div>
-        </footer>
+        </React.Fragment>
     )
 }
